6a392cf8bbf5da55291251a5079641b8c7b56d17
Debajit23 <47385207+Debajit23@users.noreply.github.com>
2019-02-06 17:31:33 +0530

    2nd method added

--- Demo.java
+++ Demo.java
@@ -3,10 +3,15 @@ class Demo1{
     int c=a+b;
     System.out.println(c);
     }
+   void deletion(int a, int b){
+    int c=a-b;
+    System.out.println(c);
+    }
  }
  class Demo{
    public static void main(String args[]){
     Demo1 ob = new Demo1();
     ob.addition(2,1);
+    ob.deletion(2,1);
     }
   }
