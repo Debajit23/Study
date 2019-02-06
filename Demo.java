3e5c26c59f9a783ec72ea273cf691048b9d51bb9
Debajit23 <47385207+Debajit23@users.noreply.github.com>
2019-02-06 17:28:23 +0530

    1st method is added

---
+++ Demo.java
@@ -0,0 +1,12 @@
+class Demo1{
+  void addition(int a, int b){
+    int c=a+b;
+    System.out.println(c);
+    }
+ }
+ class Demo{
+   public static void main(String args[]){
+    Demo1 ob = new Demo1();
+    ob.addition(2,1);
+    }
+  }
