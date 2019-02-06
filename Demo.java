6f3e6daa6480bf0d23d0a6e130aad1fbc4ca1c5d
Debajit23 <47385207+Debajit23@users.noreply.github.com>
2019-02-06 17:33:21 +0530

    3rd method added

--- Demo.java
+++ Demo.java
@@ -7,11 +7,16 @@ class Demo1{
     int c=a-b;
     System.out.println(c);
     }
+    void multiplication(int a, int b){
+    int c=a*b;
+    System.out.println(c);
+    }
  }
  class Demo{
    public static void main(String args[]){
     Demo1 ob = new Demo1();
     ob.addition(2,1);
     ob.deletion(2,1);
+    ob.multiplication(2,1);
     }
   }
